3b8e489381e515baaee3aecd01d2cdd5d0cb6379
Patricia Suriana <patricia.suriana@gmail.com>
2016-11-08 17:25:25 -0800

    Update coli test

--- test/correctness/coli.cpp
+++ test/correctness/coli.cpp
@@ -128,45 +128,7 @@ int main(int argc, char **argv) {
         C.compile_to_coli("fusion_coli.cpp", {}, "fusion_coli");
     }
 
-    // CVT Color benchmark
-    if (0) {
-        ImageParam in{UInt(8), 3, "input"};
-
-        Func RGB2Gray{"RGB2Gray"};
-        Var x("x"), y("y"), c("c");
-
-        const Expr yuv_shift = cast<uint32_t>(14);
-        const Expr R2Y = cast<uint32_t>(4899);
-        const Expr G2Y = cast<uint32_t>(9617);
-        const Expr B2Y = cast<uint32_t>(1868);
-
-
-        RGB2Gray(x, y) = cast<uint8_t>(CV_DESCALE( (in(x, y, 2) * B2Y
-                                  + in(x, y, 1) * G2Y
-                                  + in(x, y, 0) * R2Y),
-                                  yuv_shift));
-
-        RGB2Gray.parallel(y);//.vectorize(x, 8);
-
-        RGB2Gray.compile_to_coli("cvtcolor.cpp", {in}, "cvtcolor");
-    }
-
-    if (0) {
-        //ImageParam in{UInt(8), 3, "input"};
-        Image<uint8_t> in(1024, 512, 3);
-
-        Func RGB2Gray{"RGB2Gray"};
-        Var x("x"), y("y"), c("c");
-
-        RGB2Gray(x, y) = cast<uint8_t>(in(x, y, 2) + in(x, y, 1) + in(x, y, 0));
-
-        RGB2Gray.parallel(y);//.vectorize(x, 8);
-
-        RGB2Gray.realize(1024, 512);
-
-        //RGB2Gray.compile_to_coli("cvtcolor.cpp", {in}, "cvtcolor");
-    }
-
+    // Fusion benchmark
     if (0) {
         //ImageParam in{UInt(8), 3, "input"};
         Image<uint8_t> in(2124, 3540, 3);
@@ -184,26 +146,7 @@ int main(int argc, char **argv) {
         //Pipeline({f, g}).compile_to_coli("fusion_coli.cpp", {in}, "fusion_coli");
     }
 
-    if (0) {
-        ImageParam in(UInt(8), 3, "input");
-        ImageParam kernel(UInt(8), 2, "kernel");
-
-        //Image<uint8_t> in(2124, 3540, 3);
-        //Image<uint8_t> kernel(3, 3);
-
-        Func filter2D("filter2D");
-        Var x("x"), y("y"), c("c");
-        RDom r(0, kernel.width(), 0, kernel.height());
-
-        filter2D(x, y, c) += cast<uint8_t>(in(x + r.x, y + r.y, c));
-
-        filter2D.parallel(y);//.vectorize(x, 8);
-
-        //filter2D.realize(2116, 3532, 3);
-
-        filter2D.compile_to_coli("filter2D_coli.cpp", {in, kernel}, "filter2D_coli");
-    }
-
+    // Filter 2D
     if (0) {
         ImageParam in(UInt(8), 3, "input");
         ImageParam kernel(Float(32), 2, "kernel");
@@ -233,6 +176,7 @@ int main(int argc, char **argv) {
         filter2D.compile_to_coli("filter2D_coli.cpp", {in, kernel}, "filter2D_coli");
     }
 
+    // Gaussian 5x5 benchmark
     if (0) {
         ImageParam in(UInt(8), 3, "input");
         ImageParam kernelX(Float(32), 1, "kernelx");
@@ -268,6 +212,7 @@ int main(int argc, char **argv) {
         gaussian.compile_to_coli("gaussian_coli.cpp", {in, kernelX, kernelY}, "gaussian_coli");
     }
 
+    // Blur x-y
     if (0) {
         ImageParam input(UInt(8), 3);
         Func blur_x("blur_x"), blur_y("blur_y");
@@ -287,7 +232,8 @@ int main(int argc, char **argv) {
         blur_y.compile_to_coli("blurxy_coli.cpp", {input}, "blurxy_coli");
     }
 
-    if (1) {
+    // RGB to YUV420
+    if (0) {
         ImageParam rgb(Int(16), 3);
         //Image<int16_t> rgb(2124, 3540, 3);
 
@@ -308,6 +254,7 @@ int main(int argc, char **argv) {
         Pipeline({y_part, u_part, v_part}).compile_to_coli("rgbyuv420.cpp", {rgb}, "rgbyuv420");
     }
 
+    // CVT Color benchmark
     if (0) {
         //ImageParam in{UInt(8), 3, "input"};
         Image<uint8_t> in(2116, 3538, 3);
@@ -333,88 +280,28 @@ int main(int argc, char **argv) {
         //RGB2Gray.compile_to_coli("cvtcolor.cpp", {in}, "cvtcolor");
     }
 
-    if (0) {
-        ImageParam in{Float(32), 2, "input"};
-        //Image<float> in(2116, 3538, 2);
+    // Rec-filter
+    if (1) {
+        //ImageParam in(UInt(8), 3, "input"};
+        Image<uint8_t> in(2116, 3538, 3);
 
         Param<float> a0{"a0"};
         Param<float> a1{"a1"};
         Param<float> a2{"a2"};
 
         Func rec_filter{"rec_filter"};
-        Var x, y;
+        Var x("x"), y("y"), c("c");
         RDom r(2, in.width()-3, 0, in.height()-1);
 
-        rec_filter(x, y) = in(x, y);
-        rec_filter(r.x, r.y) = a0*rec_filter(r.x, r.y)
-                             + a1*rec_filter(r.x-1, r.y)
-                             + a2*rec_filter(r.x-2, r.y);
-
+        rec_filter(x, y, c) = in(x, y, c);
+        rec_filter(r.x, r.y, c) = cast<uint8_t>(a0*rec_filter(r.x, r.y, c) + a1*rec_filter(r.x-1, r.y, c) + a2*rec_filter(r.x-2, r.y, c));
 
         rec_filter.parallel(y);//.vectorize(x, 8);
         rec_filter.update(0).parallel(r.y);
 
-        //rec_filter.realize(2116, 3538);
+        rec_filter.realize(2116, 3538, 3);
 
         rec_filter.compile_to_coli("recfilter_coli.cpp", {in, a0, a1, a2}, "recfilter_coli");
-    }
-
-    // Filter 2D nordom benchmark
-    if (0) {
-        const int RADIUS = 3;
-
-        ImageParam in{Float(32), 2, "input"};
-        // kernel is 2*radius x 2*radius
-        ImageParam kernel{Float(32), 2, "kernel"};
-
-        Func filter2D_nordom{"filter2D_nordom"};
-        Var x("x"), y("y");
-
-        Expr e = 0.0f;
-
-        for (int i=-RADIUS; i<RADIUS; i++) {
-            for (int j=-RADIUS; j<RADIUS; j++)  {
-                e += in(x+RADIUS+i, y+RADIUS+j) * kernel(RADIUS+i, RADIUS+j);
-            }
-        }
-
-        filter2D_nordom(x, y) = e;
-
-        filter2D_nordom.parallel(y);//.vectorize(x, 8);
-
-        filter2D_nordom.compile_to_coli("filter2Dnordom.cpp", {in, kernel}, "filter2Dnordom");
-    }
-
-    // Gaussian 3x3 benchmark
-    if (0) {
-        const int kernelX_length = 7;
-
-        ImageParam in{Float(32), 2, "input"};
-        ImageParam kernelX{Float(32), 1, "kernelx"};
-        ImageParam kernelY{Float(32), 1, "kernely"};
-
-        Func gaussian("gaussian");
-        Func gaussian_x("gaussian_x");
-        Var x("x"), y("y");
-
-        Expr e,f;
-        e = 0.0f;
-        for (int i=0; i<kernelX_length; i++) {
-            e += in(x+i,y) * kernelX(i);
-        }
-        gaussian_x(x, y) = e;
-
-        f = 0.0f;
-        for (int i=0; i<kernelX_length; i++) {
-            f += gaussian_x(x, y+i) * kernelY(i);
-        }
-
-        gaussian(x, y) = f;
-
-        gaussian_x.compute_root();
-        gaussian.compute_root();
-
-        gaussian.compile_to_coli("gaussian3x3.cpp", {in, kernelX, kernelY}, "gaussian3x3");
     }
 
     printf("Success!\n");
